2c97ae679fd55ccf67d7bb337b46d290e4eddb49
Jose Salas <jose.salas@unosquare.com>
2018-04-27 17:10:32 -0500

    [Init] : First Commit

---
+++ CognitiveServices/CognitiveServices/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CognitiveServices.Views;
+using Xamarin.Forms;
+
+namespace CognitiveServices
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new CognitivePage());
+        }
+    }
+}
---
+++ CognitiveServices/CognitiveServices/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CognitiveServices
+{
+    public static class Constants
+    {
+        public static readonly string AuthenticationTokenEndpoint = "https://api.cognitive.microsoft.com/sts/v1.0";
+
+        //Spell Check API
+        public static readonly string BingSpellCheckApiKey = "<Here your Key>";
+        public static readonly string BingSpellCheckEndpoint = "https://api.cognitive.microsoft.com/bing/v7.0/SpellCheck";
+
+        //Text Translation API
+        public static readonly string TextTranslatorApiKey = "<Here your Key>";
+        public static readonly string TextTranslatorEndpoint = "https://api.microsofttranslator.com/v2/http.svc/translate";
+
+        // Face API
+        public static readonly string FaceApiKey = "<Here your Key>";
+        public static readonly string FaceEndpoint = "https://australiaeast.api.cognitive.microsoft.com/face/v1.0";
+
+    }
+}
